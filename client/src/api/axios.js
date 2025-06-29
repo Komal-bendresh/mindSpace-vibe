2792ae2385746c94e1fdfc6a80196259c5dc8ba9
komal-bendresh <komalbendresh1134@gmail.com>
2025-06-29 21:13:06 +0530

    added render url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-
 const instance = axios.create({
-  baseURL: "http://localhost:5000/",
-  withCredentials: true, 
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,
 });
 
 export default instance;
